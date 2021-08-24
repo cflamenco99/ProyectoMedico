4311677862fc5c51ffdf76ef1a0d186aadbda0ba
AlvaroMeza07 <alvaromeza16@gmail.com>
2021-08-24 16:51:05 -0600

    historial

--- medicos-app/src/routes.js
+++ medicos-app/src/routes.js
@@ -1,3 +1,4 @@
+import HistorialMedico from "views/examples/Historial-Medico/Historial.js";
 import Index from "views/Index.js";
 import Profile from "views/examples/Profile.js";
 import Maps from "views/examples/Maps.js";
@@ -5,6 +6,7 @@ import Register from "views/examples/Register.js";
 import Login from "views/examples/Login.js";
 import Tables from "views/examples/Tables.js";
 import Icons from "views/examples/Icons.js";
+
 
 var routes = [
   {
@@ -32,6 +34,14 @@ var routes = [
     path: "/user-profile",
     name: "User Profile",
     icon: "ni ni-single-02 text-yellow",
+    component: Profile,
+    layout: "/admin",
+  },
+
+  {
+    path: "/HistorialMedico",
+    name: "Historial Medico",
+    icon: "ni ni-single-copy-04 text-blue",
     component: Profile,
     layout: "/admin",
   },
