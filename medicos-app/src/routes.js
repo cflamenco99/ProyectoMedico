f6e0d61e67c688594a613a6fc54acbdf681fe95a
Carlos Flamenco <cflamenco99@gmail.com>
2021-08-24 18:11:26 -0600

    sidebar

--- medicos-app/src/routes.js
+++ medicos-app/src/routes.js
@@ -3,18 +3,18 @@ import Pacientes from "Pacientes/Pacientes";
 
 var routes = [
   {
-    path: "/pacientes",
-    name: "Administrar Pacientes",
-    icon: "ni ni-single-02 text-yellow",
-    component: Pacientes,
-    layout: "/admin",
-  },
-  {
     path: "/login",
     name: "Login",
     icon: "ni ni-key-25 text-info",
     component: Login,
     layout: "/auth",
+  },
+  {
+    path: "/pacientes",
+    name: "Administrar Pacientes",
+    icon: "ni ni-single-02 text-yellow",
+    component: Pacientes,
+    layout: "/admin",
   },
 ];
 export default routes;
