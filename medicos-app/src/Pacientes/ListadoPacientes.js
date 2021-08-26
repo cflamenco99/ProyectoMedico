e6920343ea7d10c0c397d79c8d3cd561e407967c
Carlos Flamenco <cflamenco99@gmail.com>
2021-08-25 18:52:51 -0600

    correcion

--- medicos-app/src/Pacientes/ListadoPacientes.js
+++ medicos-app/src/Pacientes/ListadoPacientes.js
@@ -70,9 +70,6 @@ const ListadoPacientes = () => {
                 <Row>
                   <div className="col">
                     <Card className="shadow">
-                      <CardHeader className="border-0">
-                        <h3 className="mb-0">Card tables</h3>
-                      </CardHeader>
                       <Table
                         className="align-items-center table-flush"
                         responsive
