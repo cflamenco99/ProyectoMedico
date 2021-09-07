0791a5d26119a98cfac8d00bd7d57a19f2fd4908
Carlos Flamenco <cflamenco99@gmail.com>
2021-09-07 12:39:06 -0600

    Pacientes Terminado

--- SistemaMedicoAPI/SistemaMedicoAPI/Controllers/PacientesController.cs
+++ SistemaMedicoAPI/SistemaMedicoAPI/Controllers/PacientesController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using SistemaMedicoAPI.Models;
+using SistemaMedicoAPI.Models.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,47 +12,172 @@ using System.Threading.Tasks;
 
 namespace SistemaMedicoAPI.Controllers
 {
+    [EnableCors(Startup.PolicyCORS)]
     [Route("api/[controller]")]
     [ApiController]
     public class PacientesController : ControllerBase
     {
+        private SistemaMedicoDBContext _db;
         private readonly ILogger<PacientesController> _logger;
 
-        public PacientesController(ILogger<PacientesController> logger) 
+        public PacientesController(ILogger<PacientesController> logger, SistemaMedicoDBContext db) 
         {
             _logger = logger;
+            _db = db;
         }
 
-        // GET: api/<PacientesController>
+        // GET: api/Pacientes
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<ActionResult<IEnumerable<PacienteDTO>>> ObtenerPacientes()
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                var result = from p in _db.Pacientes
+                             select new PacienteDTO
+                             {
+                                 IdPaciente = p.IdPaciente,
+                                 Nombres = p.Nombres,
+                                 Apellidos = p.Apellidos,
+                                 IdCiudad = p.IdCiudad,
+                                 CodigoPostal = p.CodigoPostal,
+                                 Direccion = p.Direccion,
+                                 FechaNacimiento = p.FechaNacimiento
+                             };               
+                return Ok(await result.ToListAsync());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return BadRequest(ex.Message);
+            }            
         }
 
-        // GET api/<PacientesController>/5
+        // GET api/Pacientes/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<PacienteDTO>> ObtenerPacientePorId(int id)
         {
-            return "value";
+            Pacientes PacienteEF = await _db.Pacientes.FindAsync(id);
+
+            if (PacienteEF != null)
+            {
+                PacienteDTO PacienteDTO = new PacienteDTO
+                {
+                    IdPaciente = PacienteEF.IdPaciente,
+                    Nombres = PacienteEF.Nombres,
+                    Apellidos = PacienteEF.Apellidos,
+                    IdCiudad = PacienteEF.IdCiudad,
+                    CodigoPostal = PacienteEF.CodigoPostal,
+                    Direccion = PacienteEF.Direccion,
+                    FechaNacimiento = PacienteEF.FechaNacimiento
+                };
+                return PacienteDTO;
+            }
+            else
+                return NotFound();
         }
 
-        // POST api/<PacientesController>
+        // POST api/Pacientes
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult<PacienteDTO>> AgregarPaciente(PacienteDTO paciente)
         {
+            try
+            {
+                Pacientes Paciente = new Pacientes
+                {
+                    IdPaciente = paciente.IdPaciente,
+                    Nombres = paciente.Nombres,
+                    Apellidos = paciente.Apellidos,
+                    IdCiudad = paciente.IdCiudad,
+                    CodigoPostal = paciente.CodigoPostal,
+                    Direccion = paciente.Direccion,
+                    FechaNacimiento = paciente.FechaNacimiento
+                };
+
+                _db.Pacientes.Add(Paciente);
+                int result = await _db.SaveChangesAsync();
+                if (result > 0)
+                {
+                    return CreatedAtAction("AgregarPaciente", new { status = "Agregado exitosamente" });
+                }
+                else
+                {
+                    return BadRequest("Ocurrio un problema al agregar el paciente");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return BadRequest(ex.Message);
+            }
         }
 
-        // PUT api/<PacientesController>/5
+        // PUT api/Pacientes/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<ActionResult<PacienteDTO>> EditarPaciente(PacienteDTO paciente)
         {
+            try
+            {
+                Pacientes pacienteEF = _db.Pacientes.Find(paciente.IdPaciente);
+                if (pacienteEF != null)
+                {
+                    pacienteEF.Nombres = paciente.Nombres;
+                    pacienteEF.Apellidos = paciente.Apellidos;
+                    pacienteEF.IdCiudad = paciente.IdCiudad;
+                    pacienteEF.CodigoPostal = paciente.CodigoPostal;
+                    pacienteEF.Direccion = paciente.Direccion;
+                    pacienteEF.FechaNacimiento = pacienteEF.FechaNacimiento;
+                    int result = await _db.SaveChangesAsync();
+                    if (result > 0)
+                    {
+                        return Ok(new { status = "Modificado exitosamente" });
+                    }
+                    else
+                    {
+                        return StatusCode(500);
+                    }
+                }
+                else
+                {
+                    return NotFound($"No hemos encontrado un paciente con el id {paciente.IdPaciente}");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return BadRequest(ex.Message);
+            }
         }
 
-        // DELETE api/<PacientesController>/5
+        // DELETE api/Pacientes/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<ActionResult<PacienteDTO>> EliminarPaciente(int id)
         {
+            try
+            {
+                Pacientes pacienteEF = _db.Pacientes.Find(id);
+                if (pacienteEF != null)
+                {
+                    _db.Pacientes.Remove(pacienteEF);
+                    int result = await _db.SaveChangesAsync();
+                    if (result > 0)
+                    {
+                        return Ok(new { status = "Eliminado exitosamente" });
+                    }
+                    else
+                    {
+                        return StatusCode(500);
+                    }
+                }
+                else
+                {
+                    return NotFound($"No hemos encontrado un paciente con el id {id}");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
---
+++ SistemaMedicoAPI/SistemaMedicoAPI/Models/DTOs/PacienteDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaMedicoAPI.Models.DTOs
+{
+    public class PacienteDTO
+    {       
+        public int IdPaciente { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public int IdCiudad { get; set; }
+        public int CodigoPostal { get; set; }
+        public string Direccion { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
