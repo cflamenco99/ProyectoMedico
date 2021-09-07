b9c1273da06ede586064ca9c263dbfccc39961d9
Carlos Flamenco <cflamenco99@gmail.com>
2021-09-07 12:02:46 -0600

    PacientesController

---
+++ SistemaMedicoAPI/SistemaMedicoAPI/Controllers/PacientesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace SistemaMedicoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PacientesController : ControllerBase
+    {
+        private readonly ILogger<PacientesController> _logger;
+
+        public PacientesController(ILogger<PacientesController> logger) 
+        {
+            _logger = logger;
+        }
+
+        // GET: api/<PacientesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<PacientesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PacientesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PacientesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PacientesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
