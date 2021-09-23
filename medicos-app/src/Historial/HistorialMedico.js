b4bdaafe7afb14dccd6772ce6d595d47151904b9
AlvaroMeza07 <alvaromeza16@gmail.com>
2021-09-23 15:19:35 -0600

    Actualizacion de Historial
    Se actualizo codigo, ya muestra lista receta y cita..
    
    Unicamente falta mostrar datos de paciente

--- medicos-app/src/Historial/HistorialMedico.js
+++ medicos-app/src/Historial/HistorialMedico.js
@@ -21,11 +21,16 @@ import {
 } from "reactstrap";
 
 import UserHeader from "components/Headers/UserHeader.js";
+import ListadoPacientes from 'Pacientes/ListadoPacientes';
 
 export default class ListaHistorial extends React.Component{
   constructor(props) {
     super(props);
-    this.state = {listaPacientes: [], inputValue: ''};
+    this.state = {listaPacientes: [], 
+      listaPaciente: [],
+      listaRecetas: [], 
+      listaCitas: [],
+      inputValue: ''};
     
     this.handleClick = this.handleClick.bind(this);
   }
@@ -44,7 +49,15 @@ export default class ListaHistorial extends React.Component{
   }
 
   handleClick(){   
-    console.log(this.state.inputValue);  
+    console.log(this.state.inputValue); 
+
+    axios.get(`https://localhost:44310/api/Historial`)
+    .then(res => {
+      const listaPaciente = res.data;
+      console.log(listaPaciente)
+      this.setState({ listaPaciente: listaPaciente });
+    });
+
      
     axios.get(`https://localhost:44310/api/Historial/citas/${this.state.inputValue}`)
     .then(res => {
@@ -104,83 +117,31 @@ render(){
                   <div className="col">
                     <Card className="shadow">
                     <div className="pl-lg-4">
-                                          <Row>
-                                            <Col lg="6">
-                                                <FormGroup>
-                                                    <label
-                                                        className="form-control-label"
-                                                    >
-                                                        Primer nombre
-                                                    </label>
-                                                    <Input
-                                                        className="form-control-alternative"
-                                                        placeholder="Primer nombre"
-                                                        type="text"
-                                                        id="primerNombre"
-                                                        readonly="readonly"   
-                                                        //onChange={}
-                                                        //value={}
-                                                    />
-                                                </FormGroup>
-                                            </Col>
-                                            <Col lg="6">
-                                                <FormGroup>
-                                                    <label
-                                                        className="form-control-label"
-                                                    >
-                                                        Segundo nombre
-                                                    </label>
-                                                    <Input
-                                                        className="form-control-alternative"
-                                                        placeholder="Segundo nombre"
-                                                        type="text"
-                                                        id="segundoNombre"
-                                                        readonly="readonly"
-                                                        //onChange={formik.handleChange}
-                                                        //value={formik.values.segundoNombre}
-                                                    />
-                                                </FormGroup>
-                                            </Col>
-                                        </Row>
-                                        <Row>
-                                            <Col lg="6">
-                                                <FormGroup>
-                                                    <label
-                                                        className="form-control-label"
-                                                    >
-                                                        Primer apellido
-                                                    </label>
-                                                    <Input
-                                                        className="form-control-alternative"
-                                                        placeholder="Primer apellido"
-                                                        type="text"
-                                                        id="primerApellido"
-                                                        readonly="readonly"
-                                                        //onChange={formik.handleChange}
-                                                        //value={formik.values.primerApellido}
-                                                    />
-                                                </FormGroup>
-                                            </Col>
-                                            <Col lg="6">
-                                                <FormGroup>
-                                                    <label
-                                                        className="form-control-label"
-                                                    >
-                                                        Segundo apellido
-                                                    </label>
-                                                    <Input
-                                                        className="form-control-alternative"
-                                                        placeholder="Segundo apellido"
-                                                        type="text"
-                                                        id="segundoApellido"
-                                                        readonly="readonly"
-                                                       // onChange={formik.handleChange}
-                                                        //value={currentValues.segundoApellido}
-                                                    />
-                                                </FormGroup>
-                                            </Col>
-                                        </Row>
+                                          
                                     </div>
+                      <Col xs="8">
+                          <h3 className="text-center-mb-0">Informacion General del Paciente</h3>
+                       </Col> 
+
+                      <Table
+                        className="align-items-center table-flush"
+                        responsive
+                      >
+                        <thead className="thead-light">
+                          <tr>
+                            <th scope="col">Nombre Completo</th>
+                            <th scope="col">Ciudad</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                        {this.state.listaPaciente.map( (currentValue, i) => 
+                        <tr key={i}>
+                        <th scope="row">{currentValue.Nombres + ' ' +currentValue.Apellidos}</th>
+                        <td>{currentValue.IdCiudad}</td>
+                        </tr>                        
+                        )}
+                        </tbody>
+                      </Table>              
 
                        <Col xs="8">
                           <h3 className="text-center-mb-0">Informacion de las Citas</h3>
@@ -197,7 +158,7 @@ render(){
                           </tr>
                         </thead>
                         <tbody>
-                        {this.state.listaPacientes.map( (currentValue, i) => 
+                        {this.state.listaCitas.map( (currentValue, i) => 
                         <tr key={i}>
                         <th scope="row">{currentValue.idCita}</th>
                         <td>{currentValue.fechaCita}</td>
@@ -222,9 +183,9 @@ render(){
                           </tr>
                         </thead>
                         <tbody>
-                        {this.state.listaPacientes.map( (currentValue, i) => 
+                        {this.state.listaRecetas.map( (currentValue, i) => 
                         <tr key={i}>
-                        <th scope="row">{currentValue.idReceta}</th>
+                        <th scope="row">{currentValue.idRecetas}</th>
                         <td>{currentValue.medicinas}</td>
                         <td>{currentValue.diagnostico}</td>
                         </tr>                        
