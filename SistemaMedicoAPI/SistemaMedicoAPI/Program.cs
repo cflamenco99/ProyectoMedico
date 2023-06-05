2b3aa686ab6e1952da5ca0298217d76d56f34252
Carlos Flamenco <57505181+cflamenco99@users.noreply.github.com>
2023-06-05 11:51:04 -0600

    Update Program.cs

--- SistemaMedicoAPI/SistemaMedicoAPI/Program.cs
+++ SistemaMedicoAPI/SistemaMedicoAPI/Program.cs
@@ -21,7 +21,7 @@ namespace SistemaMedicoAPI
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls("http://*:80", "https://*:443");
+                    webBuilder.UseUrls("http://*:80", "https://*:44310");
                 });
     }
 }
