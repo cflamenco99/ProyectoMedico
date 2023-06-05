802d127c231405916dbc5b3d8d9baebe12e43355
Carlos Flamenco <cflamenco99@gmail.com>
2023-06-05 13:09:00 -0600

    puertos

--- SistemaMedicoAPI/SistemaMedicoAPI/Program.cs
+++ SistemaMedicoAPI/SistemaMedicoAPI/Program.cs
@@ -21,6 +21,7 @@ namespace SistemaMedicoAPI
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:80", "https://*:443");
                 });
     }
 }
